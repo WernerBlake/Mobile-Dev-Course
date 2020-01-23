af1422ef73a922e4a1e6ed5a428d659016157f58
Blake Werner <wernerblake@seattleu.edu>
2020-01-23 14:01:03 -0800

    Updated MenuPage.xaml.cs Changed menu order so about is on top

--- Mine/Mine/Views/MenuPage.xaml.cs
+++ Mine/Mine/Views/MenuPage.xaml.cs
@@ -30,8 +30,8 @@ namespace Mine.Views
             // Establish the Menu List
             menuItems = new List<HomeMenuItemModel>
             {
-                new HomeMenuItemModel {Id = MenuItemEnum.Items, Title="Items" },
-                new HomeMenuItemModel {Id = MenuItemEnum.About, Title="About" }
+                new HomeMenuItemModel {Id = MenuItemEnum.About, Title="About" },
+                new HomeMenuItemModel {Id = MenuItemEnum.Items, Title="Items" }
             };
 
             // Register the ListView for the Menu and the Item Selected call back
