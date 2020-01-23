700f67c20028913c9cb3c55bfb355d4854eea6d7
Blake Werner <wernerblake@seattleu.edu>
2020-01-23 14:48:07 -0800

    GamePage, Added Go RedHawks Button

--- Mine/Mine/Views/Game/GamePage.xaml.cs
+++ Mine/Mine/Views/Game/GamePage.xaml.cs
@@ -16,5 +16,10 @@ namespace Mine.Views
         {
             InitializeComponent();
         }
+
+        async void GameButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("SU", "Go RedHawks","OK");
+        }
     }
 }
