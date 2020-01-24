49485452b65791dd8f1360ac9f1159822fa6410c
Blake Werner <wernerblake@seattleu.edu>
2020-01-23 17:31:42 -0800

    Added Stepper for Value Attribute on ItemCreate Page

--- Mine/Mine/Views/Items/ItemCreatePage.xaml.cs
+++ Mine/Mine/Views/Items/ItemCreatePage.xaml.cs
@@ -50,5 +50,10 @@ namespace Mine.Views
         {
             await Navigation.PopModalAsync();
         }
+
+        void Value_OnStepperValueChanged(object senders, ValueChangedEventArgs e)
+        {
+            ValueValue.Text = String.Format("{0}", e.NewValue);
+        }
     }
 }
