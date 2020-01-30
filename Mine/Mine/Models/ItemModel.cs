8856f038a3d6a63bffd5668b9eacfe7b70d8cb95
Blake Werner <wernerblake@seattleu.edu>
2020-01-30 11:52:07 -0800

    Added Update Method to ItemModel

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -9,5 +9,20 @@
         
         // The Value of the item
         public int Value { get; set; } = 0;
+
+        public bool Update(ItemModel data)
+        {
+            // Do not update the ID, if you do, the record will be orphaned
+            // Id = data.Id;
+
+            // Update the Base
+            Name = data.Name;
+            Description = data.Description;
+
+            // Update the extended
+            Value = data.Value;
+
+            return true;
+        }
     }
 }
