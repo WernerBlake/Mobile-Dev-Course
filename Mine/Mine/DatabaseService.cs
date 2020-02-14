070c464437b6535225166e8d7b1225a5f55e028d
Blake Werner <wernerblake@seattleu.edu>
2020-02-13 16:00:02 -0800

    Added Databaseservice, partial submit

---
+++ Mine/Mine/DatabaseService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Mine.Models;
+using System.Threading.Tasks;
+using System.Linq;
+using SQLite;
+
+namespace Mine.Services
+{
+    public class DatabaseService
+    {
+        static readonly Lazy<SQLiteAsyncConnection> lazyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
+        {
+            return new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        });
+
+        static SQLiteAsyncConnection Database => lazyInitializer.Value;
+        static bool initialized = false;
+
+        public DatabaseService()
+        {
+            InitializeAsync().SafeFireAndForget(false);
+        }
+
+        async Task InitializeAsync()
+        {
+            if (!initialized)
+            {
+                if (!Database.TableMappings.Any(m => m.MappedType.Name == typeof(ItemModel).Name))
+                {
+                    await Database.CreateTablesAsync(CreateFlags.None, typeof(ItemModel)).ConfigureAwait(false);
+                    initialized = true;
+                }
+            }
+        }
+
+        //...
+    }
+}
